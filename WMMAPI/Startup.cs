d2d03be32362eae1dfaa9920cf9cce31a7fbd9ef
Mark Taylor <mark.taylor737@gmail.com>
2021-10-27 14:39:04 -0400

    Authentication WIP

--- WMMAPI/Startup.cs
+++ WMMAPI/Startup.cs
@@ -1,12 +1,21 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
+using System;
+using System.Text;
+using System.Threading.Tasks;
 using WMMAPI.Database;
+using WMMAPI.Helpers;
 using WMMAPI.Interfaces;
 using WMMAPI.Repositories;
+
+
+
+
 
 namespace WMMAPI
 {
@@ -22,7 +31,42 @@ namespace WMMAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //services.AddCors(); //TODO Research this
             services.AddControllers();
+
+
+            // configure strongly typed settings objects
+            var appSetttingsSection = Configuration.GetSection("AppSettings");
+            services.Configure<AppSettings>(appSetttingsSection);
+
+            // configure jwt authentication
+            var appSettings = appSetttingsSection.Get<AppSettings>();
+            var key = Encoding.ASCII.GetBytes(appSettings.Secret);
+            services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(x =>
+            {
+                x.Events = new JwtBearerEvents
+                {
+                    OnTokenValidated = context =>
+                    {
+                        var userService = context.HttpContext.RequestServices.GetRequiredService<IUserRepository>();
+                        var userId = Guid.Parse(context.Principal.Identity.Name);
+                        var user = userService.GetById(userId);
+                        if (user == null)
+                        {
+                            // return unauthorized if user no longer exists
+                            context.Fail("Unauthorized");
+                        }
+                        return Task.CompletedTask;
+                    }
+                };
+            });
+
+            //TODO: Look into this authentication pattern a bit more
 
             //TODO: Add environment check to adjust db connection...
             services.AddScoped<WMMContext>();
@@ -43,6 +87,8 @@ namespace WMMAPI
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            //TODO: Add db migration here so it happens automatically when run?
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -54,6 +100,13 @@ namespace WMMAPI
 
             app.UseRouting();
 
+            // Research this
+            app.UseCors(x => x
+                .AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader());
+
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
