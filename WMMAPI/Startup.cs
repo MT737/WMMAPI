fcd774af8fe76c57ff97a8adeac6e2d32440c3bf
Mark Taylor <mark.taylor737@gmail.com>
2021-10-27 23:29:12 -0400

    Adds authorization functionality to the swagger mapping for use the swagger UI

--- WMMAPI/Startup.cs
+++ WMMAPI/Startup.cs
@@ -88,9 +88,35 @@ namespace WMMAPI
             services.AddScoped<IUserRepository, UserRepository>();
             services.AddScoped<IVendorRepository, VendorRepository>();
 
+            //TODO: Consult the swagger documentation for greater familiarity with what this is doing
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "WMMAPI", Version = "v1" });
+                OpenApiSecurityScheme securityDefinition = new OpenApiSecurityScheme()
+                {
+                    Name = "Bearer",
+                    BearerFormat = "JWT",
+                    Scheme = "bearer",
+                    Description = "Specify the authorization token.",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.Http
+                };
+                c.AddSecurityDefinition("jwt_auth", securityDefinition);
+
+                OpenApiSecurityScheme securityScheme = new OpenApiSecurityScheme()
+                {
+                    Reference = new OpenApiReference()
+                    {
+                        Id = "jwt_auth",
+                        Type = ReferenceType.SecurityScheme
+                    }
+                };
+                OpenApiSecurityRequirement securityRequirements = new OpenApiSecurityRequirement()
+                {
+                    {securityScheme, new string[] { } }
+                };
+                c.AddSecurityRequirement(securityRequirements);
+
             });
         }
 
