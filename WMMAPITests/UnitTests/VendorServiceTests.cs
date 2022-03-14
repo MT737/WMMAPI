a04ce890e70940d0db69d3c10b8fbc318afba765
Mark Taylor <mark.taylor737@gmail.com>
2022-03-14 17:15:51 -0400

    Adds VenderService unit tests

--- WMMAPITests/UnitTests/VendorServiceTests.cs
+++ WMMAPITests/UnitTests/VendorServiceTests.cs
@@ -173,7 +173,7 @@ namespace WMMAPITests.UnitTests
         {
             // Fabricate test
             User user = _testData.Users.First();
-            Vendor vendor = _testData.CreateTestVendor(true, user.Id, null, false);
+            Vendor vendor = _testData.CreateTestVendor(true, user.Id, false, null);
 
             // Initialize service and call method
             VendorService service = new VendorService(_tdc.WMMContext.Object);
@@ -187,14 +187,33 @@ namespace WMMAPITests.UnitTests
         [ExpectedException(typeof(AppException))]
         public void TestValidateVendorFailsNameExists()
         {
-            throw new NotImplementedException();
+            // Fabricate test
+            Vendor vendor = _testData.Vendors.First();
+            Vendor vendor2 = _testData.Vendors.Skip(1).First();
+            vendor2.Name = vendor.Name;
+
+            // Initialize service and call method
+            VendorService service = new VendorService(_tdc.WMMContext.Object);
+            service.ValidateVendor(vendor2);
+
+            // Verify mock and assert (nothing to assert)
+            _tdc.WMMContext.Verify(m => m.Vendors, Times.Once());
         }
 
         [TestMethod]
         [ExpectedException(typeof(AppException))]
         public void TestValidateVendorFailsNameEmptyString()
         {
-            throw new NotImplementedException();
+            // Fabricate test
+            Vendor vend = _testData.Vendors.First();
+            vend.Name = "";
+
+            // Initialize service and call method
+            VendorService service = new VendorService(_tdc.WMMContext.Object);
+            service.ValidateVendor(vend);
+
+            // Verify mock and assert
+            _tdc.WMMContext.Verify(m => m.Vendors, Times.Once());
         }
         #endregion
 
@@ -202,87 +221,204 @@ namespace WMMAPITests.UnitTests
         [TestMethod]
         public void TestGetSucceeds()
         {
-            throw new NotImplementedException();
+            // Fabricate test
+            Vendor testVend = _testData.Vendors.First();
+
+            // Initialize service and call method
+            VendorService service = new VendorService(_tdc.WMMContext.Object);
+            Vendor result = service.Get(testVend.Id, testVend.UserId);
+
+            // Confirm mock and assert
+            _tdc.WMMContext.Verify(m => m.Vendors, Times.Once());
+            Assert.AreEqual(testVend.Id, result.Id);
+            Assert.AreEqual(testVend.UserId, result.UserId);
+            Assert.AreEqual(testVend.Name, result.Name);
         }
 
         [TestMethod]
         public void TestGetDoesntExistReturnsNull()
         {
-            throw new NotImplementedException();
+            // Initialize service and call method
+            VendorService service = new VendorService(_tdc.WMMContext.Object);
+            Vendor result = service.Get(Guid.NewGuid(), Guid.NewGuid());
+
+            // Confirm mock and assert
+            _tdc.WMMContext.Verify(m => m.Vendors, Times.Once());
+            Assert.IsNull(result);
         }
 
         [TestMethod]
         public void TestGetListSucceeds()
         {
-            throw new NotImplementedException();
+            // Fabricate test
+            User user = _testData.Users.First();
+
+            // Initialize service and call method
+            VendorService service = new VendorService(_tdc.WMMContext.Object);
+            IList<Vendor> vendors = service.GetList(user.Id);
+
+            // Confirm mock and assert
+            _tdc.WMMContext.Verify(m => m.Vendors, Times.Once());
+            Assert.IsTrue(vendors.Count > 0);
+            Assert.IsFalse(vendors.Any(v => v.UserId != user.Id));
         }
 
         [TestMethod]
         public void TestGetListDoesntExistReturnsEmpty()
         {
-            throw new NotImplementedException();
+            // Fabricate test
+            Guid userId = Guid.NewGuid();
+
+            // Initialize service and call method
+            VendorService service = new VendorService(_tdc.WMMContext.Object);
+            IList<Vendor> vendors = service.GetList(userId);
+
+            // Confirm mock and assert
+            _tdc.WMMContext.Verify(m => m.Vendors, Times.Once());
+            Assert.IsTrue(vendors.Count == 0);
         }
 
         [TestMethod]
-        public void TestAddVendor()
+        public void TestAddVendorSucceeds() // Failures covered in validation tests
         {
-            throw new NotImplementedException();
+            // Fabricate test
+            User user = _testData.Users.First();
+            Vendor vendor = _testData.CreateTestVendor(true, user.Id, false, "TestAddVendor");
 
-            // Failures covered in validation tests
+            // Initialize service and call method
+            VendorService service = new VendorService(_tdc.WMMContext.Object);
+            service.AddVendor(vendor);
+
+            // Confirm mock and assert (no assert required, just confirm mock)
+            _tdc.WMMContext.Verify(m => m.Vendors, Times.Once());
+            _tdc.VendorSet.Verify(m => m.Add(It.IsAny<Vendor>()), Times.Once());
+            _tdc.WMMContext.Verify(m => m.SaveChanges(), Times.Once());            
         }
 
         [TestMethod]
         public void TestModifyVendorSucceeds()
         {
-            throw new NotImplementedException();
+            // Fabricate test
+            Vendor vend = _testData.Vendors.First(v => !v.IsDefault);
+            vend.Name = "Modified name";
+            vend.IsDisplayed = !vend.IsDisplayed;
+
+            // Initialize service and call method
+            VendorService service = new VendorService(_tdc.WMMContext.Object);
+            service.ModifyVendor(vend);
+
+            // Confirm mock and assert (no assert necessary, just confirm mock)
+            _tdc.WMMContext.Verify(m => m.Vendors, Times.Exactly(2));
+            _tdc.WMMContext.Verify(m => m.SaveChanges(), Times.Once());
         }
 
         [TestMethod]
         [ExpectedException(typeof(AppException))]
         public void TestModifyVendorVendorDoesntExist()
         {
-            throw new NotImplementedException();
+            // Fabricate test
+            Vendor vend = _testData.CreateTestVendor(true, Guid.NewGuid());
+            vend.Name = "modified Name";
+
+            // Initialize service and call method
+            VendorService service = new VendorService(_tdc.WMMContext.Object);
+            service.ModifyVendor(vend);
+
+            // Confirm mock and assert (no assert necessary, just confirm mock)
+            _tdc.WMMContext.Verify(m => m.Vendors, Times.Once());
         }
 
         [TestMethod]
         [ExpectedException(typeof(AppException))]
         public void TestModifyVendorDefault()
         {
-            throw new NotImplementedException();
+            // Fabricate test
+            Vendor vend = _testData.Vendors.First(v => v.IsDefault);
+            vend.Name = "Modified name";
+
+            // Initialize service and call method
+            VendorService service = new VendorService(_tdc.WMMContext.Object);
+            service.ModifyVendor(vend);
+
+            // Confirm mock and assert
+            _tdc.WMMContext.Verify(m => m.Vendors, Times.Once());
         }
 
         [TestMethod]
         public void TestDeleteVendorSucceeds()
         {
-            throw new NotImplementedException();
+            // Fabricate test
+            User user = _testData.Users.First();
+            List<Vendor> vendors = _testData.Vendors.Where(v => !v.IsDefault && v.UserId == user.Id).Take(2).ToList();
+            Vendor absorb = vendors.First();
+            Vendor absorbed = vendors.Last();
+
+            // Initialize service and call method
+            _tdc.VendorSet.Setup(m => m.Find(It.IsAny<Guid>())).Returns(absorbed);
+            VendorService service = new VendorService(_tdc.WMMContext.Object);
+            service.DeleteVendor(absorbed.Id, absorb.Id, user.Id);
+
+            // Confirm mock and assert (no need to assert, just confirm mock)
+            _tdc.WMMContext.Verify(m => m.Vendors, Times.Exactly(2));
+            _tdc.WMMContext.Verify(m => m.Transactions, Times.Once());
+            _tdc.WMMContext.Verify(m => m.SaveChanges(), Times.Exactly(2)); // TODO Need to return to this and settle on a single save call
+            _tdc.VendorSet.Verify(m => m.Find(absorbed.Id), Times.Once());
+            _tdc.VendorSet.Verify(m => m.Remove(absorbed), Times.Once());
         }
 
         [TestMethod]
         [ExpectedException(typeof(AppException))]
-        public void TestDeleteVendorVendorDoesntExist()
+        public void TestDeleteAbsorbVendorDoesntExist()
         {
-            throw new NotImplementedException();
+            // Fabricate test
+            User user = _testData.Users.First();
+            Vendor absorb = _testData.CreateTestVendor(true, user.Id, false);
+            Vendor absorbed = _testData.Vendors.First(v => !v.IsDefault && v.UserId == user.Id);
+
+            // Initialize service and call method
+            VendorService service = new VendorService(_tdc.WMMContext.Object);
+            service.DeleteVendor(absorbed.Id, absorb.Id, user.Id);
+
+            // Confirm mock and assert ( no need to confirm mock or assert)
         }
 
         [TestMethod]
         [ExpectedException(typeof (AppException))]
         public void TestDeleteVendorDefaultVendor()
         {
-            throw new NotImplementedException();
+            // Fabricate test
+            User user = _testData.Users.First();
+            Vendor absorb = _testData.Vendors.First(v => v.IsDefault && v.UserId == user.Id);
+            Vendor absorbed = _testData.Vendors.Where(v => v.IsDefault && v.UserId == user.Id).Skip(1).First();
+
+            // Initialize service and call method
+            VendorService service = new VendorService(_tdc.WMMContext.Object);
+            service.DeleteVendor(absorbed.Id, absorb.Id, user.Id);
+
+            // Confirm mock and assert (no need to confirm mock or assert)
         }
 
         [TestMethod]
         [ExpectedException(typeof (AppException))]
-        public void TestDeleteAbsorbingVendorDoesntExist()
+        public void TestDeleteAbsorbedVendorDoesntExist()
         {
-            throw new NotImplementedException();
+            // Fabricate test
+            User user = _testData.Users.First();
+            Vendor absorb = _testData.Vendors.First(v => v.IsDefault && v.UserId == user.Id);
+            Vendor absorbed = _testData.CreateTestVendor(true, user.Id, false);
+
+            // Initialize service and call method
+            VendorService service = new VendorService(_tdc.WMMContext.Object);
+            service.DeleteVendor(absorbed.Id, absorb.Id, user.Id);
+
+            // Confirm mock and assert (no need to confirm mock or assert)
         }
         #endregion
 
         #region PrivateHelpers
         private Vendor GenerateVendorWithTransactions(string vendName, Guid userId, bool isDefault)
         {
-            Vendor vend = _testData.CreateTestVendor(true, userId, vendName,  isDefault);
+            Vendor vend = _testData.CreateTestVendor(true, userId, isDefault, vendName);
             _testData.Vendors = _testData.Vendors.Concat(new List<Vendor> { vend });
 
             List<Transaction> transactions = new();
