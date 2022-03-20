80ce8a764d02442c04e2d158b911020a268f0abd
Mark Taylor <mark.taylor737@gmail.com>
2022-03-20 17:35:02 -0400

    Account Controller unit test WIP

--- WMMAPI/Controllers/AccountController.cs
+++ WMMAPI/Controllers/AccountController.cs
@@ -16,20 +16,29 @@ namespace WMMAPI.Controllers
         private readonly ILogger<AccountController> _logger;
         private readonly IAccountService _accountService;
 
+        public Guid UserId { get; set; }
+
         public AccountController(ILogger<AccountController> logger, IAccountService accountService)
         {
             _logger = logger;
             _accountService = accountService;
+
+            // TODO This shouldn't be possible, but it's needed for testing. Re-think testing?
+            UserId = User != null ? Guid.Parse(User.Identity.Name) : Guid.Empty;
         }
 
         [HttpGet]
         public IActionResult GetAccounts()
         {
+            // TODO This shouldn't be a possibility, but it's needed for testing. Re-think testing?
+            if (UserId == Guid.Empty)
+                return BadRequest(new { message = "Authentication failure" });
+
             try
             {
                 // Get list of accounts
                 var accountsWithBalance = _accountService
-                    .GetList(Guid.Parse(User.Identity.Name));
+                    .GetList(UserId);
 
                 return Ok(accountsWithBalance);
             }
@@ -41,10 +50,13 @@ namespace WMMAPI.Controllers
 
         [HttpPost]
         public IActionResult AddAccount([FromBody] AddAccountModel model)
-        {            
+        {
+            if (UserId == Guid.Empty)
+                return BadRequest(new { message = "Authentication failure" });
+
             try
             {
-                var account = model.ToDB(Guid.Parse(User.Identity.Name));
+                var account = model.ToDB(UserId);
                 _accountService.AddAccount(account);
 
                 //TODO: Need to add functionality for setting initial balance                
@@ -59,9 +71,12 @@ namespace WMMAPI.Controllers
         [HttpPut]
         public IActionResult ModifyAccount([FromBody] UpdateAccountModel model)
         {
+            if (UserId == Guid.Empty)
+                return BadRequest(new { message = "Authentication failure" });
+
             try
             {
-                var account = model.ToDB(Guid.Parse(User.Identity.Name));
+                var account = model.ToDB(UserId);
                 _accountService.ModifyAccount(account);
 
                 //TODO: Add functionality for adjusting balance
--- WMMAPITests/UnitTests/ControllerTests/AccountControllerTests.cs
+++ WMMAPITests/UnitTests/ControllerTests/AccountControllerTests.cs
@@ -1,16 +1,62 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Net;
+using WMMAPI.Controllers;
+using WMMAPI.Interfaces;
+using WMMAPI.Models.AccountModels;
+using WMMAPITests.DataHelpers;
 
 namespace WMMAPITests.UnitTests.ControllerTests
 {
     [TestClass]
     public class AccountControllerTests
     {
+        private TestData _td;
+        private Mock<ILogger<AccountController>> _mockLogger;
+        private Mock<IAccountService> _mockAccountService;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _td = new TestData();
+            _mockLogger = new Mock<ILogger<AccountController>>();
+            _mockAccountService = new Mock<IAccountService>();
+        }
+
+        [TestMethod]
+        public void GetShouldReturnViewModel()
+        {            
+            var userId = Guid.NewGuid();
+            var testAccounts = GenerateTestAccountModels(userId);
+            _mockAccountService.Setup(m => m.GetList(userId)).Returns(testAccounts);
+
+            AccountController controller = new(_mockLogger.Object, _mockAccountService.Object);
+            controller.UserId = userId;
 
 
+            OkObjectResult result = (OkObjectResult)controller.GetAccounts();
+            
+
+            Assert.IsNotNull((int)result.StatusCode == (int)HttpStatusCode.OK);
+            Assert.IsInstanceOfType(result.Value, typeof(IList<AccountModel>));
+            IList<AccountModel> accounts = (IList<AccountModel>)result.Value;
+            Assert.AreEqual(5, accounts.Count);
+        }
+
+        #region TestHelpers
+        private IList<AccountModel> GenerateTestAccountModels(Guid userId)
+        {            
+            IList<AccountModel> models = new List<AccountModel>();
+            for (int i = 0; i < 5; i++)
+            {
+                models.Add(new AccountModel(_td.CreateTestAccount(userId)));
+            }
+            return models;
+        }
+        #endregion
     }
 }
